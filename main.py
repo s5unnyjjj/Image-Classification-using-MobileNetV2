af7a117867cc140e2c3496870366dce354081e59
s5unnyjjj <s5unnyjjj@gmail.com>
2024-05-05 22:43:59 +0900

    Execute the project

---
+++ main.py
@@ -0,0 +1,25 @@
+
+
+from evaluate import evaluation
+from train import training
+import os
+import setting
+
+
+def start_training(dir):
+    model = training(data_dir=dir)
+    return model
+
+
+def start_evaluation(dir, model):
+    evaluation(path=os.path.join(dir, 'val'), model=model)
+
+
+if __name__ == '__main__':
+    base_dir = setting.Base_path
+
+    model = start_training(dir=base_dir)
+    print('[Completed] Training')
+
+    start_evaluation(dir=base_dir, model=model)
+    print('[Completed] Evaluation')
